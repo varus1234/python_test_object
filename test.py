1d45078af60f68fc7bb579b3e70ebbb7270a00d7
varus1234 <varus.46824.varus@gmail.com>
2020-12-17 10:21:29 +0000

    modify test.py

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
 """
 このファイルに解答コードを書いてください
 """
+
+with open('input.txt.txt') as f:
+    s = f.read()
+    s = s.split('\n')
+    m = float(s[-2]) #floatだと30ケタまで表示されていない？
+    s = s[:-2]
+    s.sort()
+
+dict_is = {}
+for i in s:
+    key = i.split(':')[0]
+    key = int(key)
+    value = i.split(':')[1]
+    dict_is[key] = value
+
+answer = ''
+for i, s in dict_is.items():
+    if m%i == 0:
+        answer += s
+
+if answer == '':
+    answer = m
+
+print(answer)
